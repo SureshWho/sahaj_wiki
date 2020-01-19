bb7a7fcc6f29b41043a878b0b5debce1649094a5
SureshWho <naanyaar@gmail.com>
2020-01-19 12:21:53 +0530

    Initial Commit

---
+++ sahaj_wiki/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sahaj_wiki
+//
+//  Created by Ezhil Nithya Suresh on 19/01/20.
+//  Copyright © 2020 Ezhil Nithya Suresh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
