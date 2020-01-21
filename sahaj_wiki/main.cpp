b38e0967324b727f7c050d000f08e18b1456becf
SureshWho <naanyaar@gmail.com>
2020-01-21 12:46:17 +0530

    Added weight findings

--- sahaj_wiki/main.cpp
+++ sahaj_wiki/main.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <string>
 #include <string.h>
+#include <math.h>
 
 #include "main.hpp"
 
@@ -19,6 +20,8 @@ char input_file[256] = "/Volumes/MyMedias/MachineLearning/code/code/sahaj_wiki/s
 
 int main(int argc, const char * argv[]) {
     comprehension com;
+    int ans, i, story;
+    float weight, max_weight = 0;
     
     com.init(input_file);
     std::cout << "0 " << com.story[0] << "\n";
@@ -26,8 +29,37 @@ int main(int argc, const char * argv[]) {
     std::cout << "6 " << com.story[6] << "\n";
     std::cout << "Last " << com.story[com.story_sz()-1] << "\n";
     
-    std::cout << "Q1 " << com.questions[4] << "\n";
-    std::cout << "A1 " << com.answers[4] << "\n";
+    std::cout << "Q1 " << com.questions[0] << "\n";
+    std::cout << "A1 " << com.answers[0] << "\n";
+    
+    ans = 0;
+    for (i = 0; i < com.story_sz(); i++)
+    {
+        weight = com.questions[0].compare(com.story[i]);
+        if (weight > max_weight)
+        {
+            ans = i;
+            max_weight = weight;
+        }
+        std::cout << i << " " << weight << "\n";
+    }
+    
+    cout << "ANS : " << ans << " " << com.story[ans];
+    story = ans;
+    ans = 0;
+    max_weight = 0;
+    for (i = 0; i < com.answers_sz(); i++)
+    {
+        weight = com.story[story].compare(com.answers[i]);
+        if (weight > max_weight)
+        {
+            ans = i;
+            max_weight = weight;
+        }
+        std::cout << i << " " << weight << "\n";
+    }
+    
+    cout << "ANS : " << ans << " " << com.answers[ans];
     
     return 0;
 }
@@ -57,6 +89,128 @@ int sentence::parse(const std::string &str, sentence *psentences,  int max_sente
         psentences[i++] = str.substr(last_pos, cur_pos - last_pos);
     
     return i;
+}
+
+float sentence::compare(const sentence &target) const
+{
+    int    len_src, len_des;
+    string src[SEN_MAX_WORDS], des[SEN_MAX_WORDS];
+    
+    len_src = get_words(src, SEN_MAX_WORDS);
+    len_des = target.get_words(des, SEN_MAX_WORDS);
+    
+    //printf ("Source %d Des %d", len_src, len_des);
+    
+    return compare_words(src, len_src, des, len_des);
+}
+
+int sentence::sizeinwords() const
+{
+    int wc = 0;
+
+    for (int i = 0; i < length(); i++)
+        if (at(i) == ' ')
+            wc++;
+
+    return wc;
+}
+
+float sentence::compare_words(string *p_src_words, int src_words, string *p_des_words, int des_words)
+{
+    int    src_index, des_index;
+    float  max_weight, weight, matches = 0.0;
+    
+    matches    = 0;
+    max_weight = 0.0;
+    
+    for (src_index = 0; src_index < src_words; src_index++)
+    {
+        max_weight = 0.0;
+        for (des_index = 0; des_index < des_words; des_index++)
+        {
+            weight = compare_word(p_src_words[src_index], p_des_words[des_index]);
+            max_weight = max(max_weight, weight);
+        }
+        matches += max_weight;
+    }
+    
+    printf ("Mathes %f Src Words %d\n", matches, src_words);
+    
+    return matches / src_words ;
+}
+
+int sentence::get_words(string *p_words, int max_words) const
+{
+    int    num_of_words = 0;
+    size_t pos = 0;
+    string s;
+    
+    //cout << "SENTANCE " << *this << ": ";
+    
+    while ((pos < length() ) && (num_of_words < max_words))
+    {
+        s = get_word(pos);
+        p_words[num_of_words++] = s;
+        //cout << s << " " << pos << " ";
+    }
+    
+    //cout << "\n" ;
+    
+    return num_of_words;
+}
+
+float sentence::compare_word(string src, string des)
+{
+    size_t src_len, des_len, pos, len;
+    int    matches = 0;
+    float  weight;
+    
+    /* for all common words return zero as weights */
+    pos = COMMON_WORDS.find(src);
+    if (pos != npos)
+        return 0;
+    
+    src_len = src.length();
+    des_len = des.length();
+    len     = min(src_len, des_len);
+
+    for (pos = 0; pos < len; pos++)
+        if (tolower(src.at(pos)) == tolower(des.at(pos)))
+            matches++;
+    weight = (float) matches / src_len;
+    weight = round(weight - 0.2);
+    
+    cout << "weight " << src << " " << des << " " << weight << "\n";
+    
+    return weight;
+}
+
+string sentence::get_word(size_t &pos) const
+{
+    string word;
+    int    state = END;
+    size_t last  = length() - 1;
+    
+    word = "";
+    while (pos <= last)
+    {
+       /* if it is not space add the chars into the word*/
+        if (at(pos) != ' ')
+        {
+            word  = word + at(pos);
+            state = MIDDLE;
+        }
+        
+        /* if white space and previous state was middle of a word */
+        if ((at(pos) == ' ') && (state == MIDDLE))
+        {
+            return word;
+        }
+        
+        /* go to next char */
+        pos++;
+    }
+    return word;
 }
 
 /* Read the given comprehension file and make the comprehension object
@@ -141,4 +295,9 @@ bool comprehension::init (const char *pFileName)
 int comprehension::story_sz() const
 {
     return sz_story;
+}
+
+int comprehension::answers_sz() const
+{
+    return sz_answers;
 }
--- sahaj_wiki/main.hpp
+++ sahaj_wiki/main.hpp
@@ -9,6 +9,10 @@
 #ifndef main_h
 #define main_h
 
+using namespace std;
+
+#define SEN_MAX_WORDS    64
+
 class sentence : public std::string
 {
     public :
@@ -19,9 +23,26 @@ class sentence : public std::string
     using std::string::string;
     using std::string::operator=;
     
+    float compare(const sentence &st) const;
+    string get_word(size_t &pos) const;
+    int get_words(string *p_words, int max_words) const;
+    int sizeinwords() const;
+    
     /* prase the given lines into mltiple sentences */
     static int parse(const std::string &str, sentence *psentences,  int max_sentences, char delimeter);
+    
+protected:
+    static const int MIDDLE;
+    static const int END;
+    static const string COMMON_WORDS;
+    static float compare_words(string *p_src_words, int src_words, string *p_des_words, int des_words);
+    static float compare_word(string src_word, string des_words);
+    
 };
+
+const int sentence::MIDDLE     = 1;
+const int sentence::END = 0;
+const string sentence::COMMON_WORDS = "are which and the what";
 
 /*
  *
@@ -38,6 +59,7 @@ public:
     
     bool init (const char *pFileName);
     int story_sz() const;
+    int answers_sz() const;
     
 private:
     int init_sentences(std::ifstream &fin, sentence *psentences, char delimeter);
